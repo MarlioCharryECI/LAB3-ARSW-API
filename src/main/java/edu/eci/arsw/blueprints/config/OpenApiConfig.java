51fdaeb912d5ae34a83f92ff4dc54aabb030759b
Marlio <marlio.charry-e@mail.escuelaing.edu.co>
2026-02-11 16:57:00 -0500

    Documentacion automatica

--- src/main/java/edu/eci/arsw/blueprints/config/OpenApiConfig.java
+++ src/main/java/edu/eci/arsw/blueprints/config/OpenApiConfig.java
@@ -20,27 +20,7 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title(" ARSW Blueprints API")
                         .description("API RESTful para gestionar planos arquitectónicos con persistencia en PostgreSQL")
-                        .version("v1.0")
-                        .contact(new Contact()
-                                .name("ARSW Lab 4 - Equipo")
-                                .email("equipo@arsw.edu")
-                                .url("https://github.com/arsw/blueprints"))
-                        .license(new License()
-                                .name("MIT License")
-                                .url("https://opensource.org/licenses/MIT")))
-                .servers(List.of(
-                        new Server()
-                                .url("http://localhost:8080")
-                                .description("Servidor de desarrollo - Docker local"),
-                        new Server()
-                                .url("https://api.blueprints.com")
-                                .description("Servidor de producción")))
-                .tags(List.of(
-                        new Tag()
-                                .name("Blueprints")
-                                .description("Operaciones CRUD para gestión de planos"),
-                        new Tag()
-                                .name("Authors")
-                                .description("Consultas de planos por autor")));
+                        .version("v1.0"));
+
     }
 }
